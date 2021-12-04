d013426b56a09e6217807a418a4a3396a48f23fe
Presy Lord Kidlat <91861180+presylord@users.noreply.github.com>
2021-12-05 01:46:03 +0800

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,27 @@ function App() {
     email: ""
   });
 
+  function onEntry(event) {
+    const { name, value } = event.target;
+    setContact(function (previous) {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: previous.lName
+        };
+      } else if (name === "lName") {
+        return {
+          fName: previous.fName,
+          lName: value
+        };
+      } else if (name === "email") {
+        return {
+          email: value
+        };
+      }
+    });
+  }
+
   return (
     <div className="container">
       <h1>
@@ -14,9 +35,9 @@ function App() {
       </h1>
       <p>{contact.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+        <input name="fName" placeholder="First Name" onChange={onEntry} />
+        <input name="lName" placeholder="Last Name" onChange={onEntry} />
+        <input name="email" placeholder="Email" onChange={onEntry} />
         <button>Submit</button>
       </form>
     </div>
